33edb454f5b999aad741564598832b121335e2a6
auzhye <onenegev@gmail.com>
2023-11-07 16:34:09 +0200

    v2

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,14 +4,7 @@ import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, signO
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAfqUiHocer4GsfI8z-c-vCjpUfSANnVSY",
-    authDomain: "skola-30019.firebaseapp.com",
-    databaseURL: "https://skola-30019-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "skola-30019",
-    storageBucket: "skola-30019.appspot.com",
-    messagingSenderId: "838616100299",
-    appId: "1:838616100299:web:df4c17e42219dc80a99a4e",
-    measurementId: "G-KF4CVWBKE4"
+
   };
 
 // Initialize Firebase
